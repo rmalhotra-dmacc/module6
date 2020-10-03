b15511cc716ac3f48d7c77f6037ac15f637e887a
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-10-03 15:21:06 -0500

    Added validate_input_in_functions.py and test_valid_input_in_functions.py

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,24 @@
+"""
+Author: Rajiv Malhotra
+Program: validate_input_in_functions.py
+Date: 10/03/20
+
+Program takes a test_name, test_score, and invalid_message that validates the test_score,
+then prints valid input as 'Test name: ##'.
+"""
+
+
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """
+    Function that accepts params and returns a string
+    :param test_name: The name of a test exam
+    :param test_score: A value between 0 and 100 representing a test score
+    :param invalid_message: Message displayed when an invalid score is provided
+    :return: formatted string with test name and test score.
+    """
+    # return { test_name: test_score}
+    pass
+
+
+if __name__ == '__main__':
+    print(score_input("test1", 85))
---
+++ test_functions/test_valid_input_in_functions.py
@@ -0,0 +1,27 @@
+import unittest
+from more_functions import validate_input_in_functions as val_input
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual("Python1!: 0", val_input.score_input("Python1!"))
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual("Python2!: 95", val_input.score_input("Python2!", 95))
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual("Invalid test score, try again!", val_input.score_input("Python3!, -5"))
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual("Invalid test score, try again!", val_input.score_input("Python4!, 125"))
+
+    def test_test_score_non_numeric(self):
+        with self.assertRaises(TypeError):
+            val_input.score_input("Python5!", "Ninty")
+
+    def  test_score_input_invalid_message(self):
+        self.assertEqual("Python6!: 90", val_input.score_input("Python6!", 90, "Not a valid score"))
+
+
+if __name__ == '__main__':
+    unittest.main()
