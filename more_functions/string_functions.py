77c88d2fcb8085c5eb2685ee427d6ccf5236ac89
Rajiv Malhotra <malhotra.rajiv@principal.com>
2020-10-03 13:54:25 -0500

    Multiply_string & test_string_functions logic added.

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -7,8 +7,21 @@ The program will accept string message and a number n and returns the string wit
 """""
 
 
-def multiply_string():
-    pass
+def multiply_string(message, n):
+    """
+    This function takes a message and number and returns the message printed n times
+    :param message: string
+    :param n: integer
+    :return: string
+    """
+    try:
+        #check if number is between 1 and 9
+        if 1 <= n <= 9:
+            return(message * n)
+        else:
+            print("Value of n must be between 1 and 9")
+    except TypeError as err:
+        print(err)
 
 
 if __name__ == '__main__':
